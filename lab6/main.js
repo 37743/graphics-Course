de2bccf74873090ece5c05fb1ecd16ac3386ea12
37743 <yousef.ibrahim.gomaa@gmail.com>
2023-11-16 13:18:37 +0200

    Updated labs' files

--- lab6/main.js
+++ lab6/main.js
@@ -6,13 +6,20 @@ if(!gl)
     alert("WebGl is NOT available.")
 }
 
-const vertexData = [0,1,0,
-                    1,-1,0,
-                    -1,-1,0];
+const vertexData = [];
+const r = .5;
+const lines = 100;
+
+for (let i = 0; i <= lines; i++) {
+    const theta = 2*(i / lines)*Math.PI;
+    const x = r*Math.cos(theta);
+    const y = r*Math.sin(theta);
+    vertexData.push(x, y, 0.0);
+}
 
 const buffer = gl.createBuffer();
 gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
-gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertexData), gl.DYNAMIC_DRAW);
+gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertexData), gl.STATIC_DRAW);
 
 const vertexShader = gl.createShader(gl.VERTEX_SHADER);
 
@@ -34,6 +41,7 @@ gl.shaderSource(fragmentShader,`
         gl_FragColor = vec4(1,0,0,1);
     }
 `);
+
 gl.compileShader(fragmentShader);
 
 const program = gl.createProgram();
@@ -42,9 +50,13 @@ gl.attachShader(program, vertexShader);
 gl.attachShader(program, fragmentShader);
 gl.linkProgram(program)
 
+// EXPLAIN THE 3 LINES BELOW
 const positionLocation = gl.getAttribLocation(program, `position`);
 gl.enableVertexAttribArray(positionLocation);
 gl.vertexAttribPointer(positionLocation,3,gl.FLOAT,false,0,0);
 
+gl.clearColor(0, 0, 0, 1);
+gl.clear(gl.COLOR_BUFFER_BIT);
+
 gl.useProgram(program);
-gl.drawArrays(gl.TRIANGLES, 0, 3)
+gl.drawArrays(gl.TRIANGLE_FAN, 0, lines);
